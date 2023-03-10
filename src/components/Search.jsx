d3a80f7357ba75bfa1d095c0e974b8f8e16bb276
jorgeibarra87 <jorgeibarra87@gmail.com>
2023-03-10 16:26:48 -0500

    add automatic search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react'
 import styles from "../pages/Search.module.css"
 import { ImSearch } from 'react-icons/im'
 import { useNavigate } from "react-router"
@@ -9,22 +8,19 @@ export default function Search() {
     const query = useQuery();
     const search = query.get("search")
 
-    const [searchText, setSearchText] = useState()
     const navigate = useNavigate();
-
-    useEffect(() => {
-        setSearchText(search || "")
-    }, [search]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        navigate("/?search=" + searchText)
 
     }
     return (
         <form className={styles.searchContainer} onSubmit={handleSubmit}>
             <div className={styles.searchBox}>
-                <input className={styles.searchInput} type="text" placeholder='Search movies' value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+                <input className={styles.searchInput} type="text" placeholder='Search movies' value={search} onChange={(e) => {
+                    const value = e.target.value;
+                    navigate("/?search=" + value);
+                }} />
                 <button className={styles.searchButton} type="submit">
                     <ImSearch size={20} />
                 </button>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import Search from '../components/Search.jsx'
 import { MoviesGrid } from '../components/MoviesGrid.jsx'
 import { useQuery } from '../hooks/useQuery';
+import { useDebounce } from '../hooks/useDebounce.jsx';
 
 export function LandingPage() {
 
     const query = useQuery();
     const search = query.get("search")
 
+    const debounceSearch = useDebounce(search, 500)
+
     return (
         <div>
             <Search />
-            <MoviesGrid key={search} search={search} />
+            <MoviesGrid key={debounceSearch} search={debounceSearch} />
         </div>
 
     )
