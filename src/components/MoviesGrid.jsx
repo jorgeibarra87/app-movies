bcd75ce7864d6899c48a306928784dac154403ad
jorgeibarra87 <jorgeibarra87@gmail.com>
2023-03-10 13:56:38 -0500

    add infinite scroll

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -4,33 +4,37 @@ import { get } from "../utils/httpClient"
 import styles from "../MoviesGrid.module.css"
 import Spinner from '../components/Spinner';
 import { useQuery } from '../hooks/useQuery';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-export function MoviesGrid() {
+export function MoviesGrid({ search }) {
     const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(true)
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasmotre] = useState(true)
 
-    const query = useQuery();
-    const search = query.get("search")
 
     useEffect(() => {
         setIsLoading(true);
-        const searchUrl = search ? "/search/movie?query=" + search : "/discover/movie";
+        const searchUrl = search
+            ? "/search/movie?query=" + search + "&page=" + page
+            : "/discover/movie?page=" + page;
         get(searchUrl).then((data) => {
             "/discover/movie"
-            setMovies(data.results);
+            setMovies((prevMovies) => prevMovies.concat(data.results));
+            setHasmotre(data.page < data.total_pages)
             setIsLoading(false)
         });
-    }, [search]);
+    }, [search, page]);
 
-    if (isLoading) {
-        return <Spinner />
-    }
+
 
     return (
-        <ul className={styles.moviesGrid}>
-            {movies.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} />
-            ))}
-        </ul>
+        <InfiniteScroll dataLength={movies.length} hasMore={hasMore} next={() => setPage((prevPage) => prevPage + 1)} loader={<Spinner />}>
+            <ul className={styles.moviesGrid}>
+                {movies.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </ul>
+        </InfiniteScroll>
     )
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Search from '../components/Search.jsx'
 import { MoviesGrid } from '../components/MoviesGrid.jsx'
+import { useQuery } from '../hooks/useQuery';
 
 export function LandingPage() {
+
+    const query = useQuery();
+    const search = query.get("search")
+
     return (
         <div>
             <Search />
-            <MoviesGrid />
+            <MoviesGrid key={search} search={search} />
         </div>
 
     )
